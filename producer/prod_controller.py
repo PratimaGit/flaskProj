fd2f02592fdf4d088a496552d257c80f9407c13d
hakepg <pratimaghake@gmail.com>
2019-11-21 18:24:03 +0530

    first commit

---
+++ producer/prod_controller.py
@@ -0,0 +1,60 @@
+from producer.prod_service import *
+from flask import request
+import json
+
+
+db.create_all()
+strservice = prod_service()
+
+@producer.route('/welcome/') #http://127.0.0.1:5001/welcome/
+def welcome_to_producer():
+    return 'producer invoked..!!'
+
+@producer.route('/service/', methods = ['POST']) #http://127.0.0.1:5001/service
+def save_student():
+    reqjson = request.get_json()
+    print(reqjson)
+    print(type(reqjson))
+    st = Student(**reqjson)
+    return strservice.add_prod(st)
+
+@producer.route('/service/', methods = ['PUT'])
+def update_student():
+    reqjson = request.get_json()
+    print(reqjson)
+    print(type(reqjson))
+    stu = Student(**reqjson)
+    return strservice.update_prod(stu)
+
+@producer.route('/service/<int:sid>', methods=['DELETE'])
+def delete_student(sid):
+    return strservice.delete_prod(sid)
+
+@producer.route('/service/<int:sid>', methods = ['GET'])
+def get_student(sid):
+    student = strservice.get_prod(sid)
+    print(student.__dict__)
+
+
+    stud_dict = {}
+    student.__dict__.pop('_sa_instance_state')
+    stud_dict[student.id] = student.__dict__ #print by id
+    return json.dumps(stud_dict)  # convert python object to json object
+
+@producer.route('/service/',methods = ['GET'])
+def get_all_student():
+    allstud=strservice.get_all_prod()
+    print(allstud)
+
+    dictOfStuds = {}
+    for st in allstud:
+        st.__dict__.pop('_sa_instance_state')
+        dictOfStuds[st.id] = st.__dict__
+    return json.dumps(dictOfStuds)
+
+
+
+if __name__ == '__main__':
+    producer.run(debug = True,port=5001)
+    save_student()
+
